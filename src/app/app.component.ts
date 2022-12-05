1244a87e44cd32f2425fed7223e6abcbefefdbae
Mohamed Fairoj <fairojm@gmail.com>
2022-12-05 20:52:59 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-project';
+  userList = [{"id":1,"name":"Leanne Graham","username":"Bret","email":"Sincere@april.biz"},{"id":2,"name":"Ervin Howell","username":"Antonette","email":"Shanna@melissa.tv"},{"id":3,"name":"Clementine Bauch","username":"Samantha","email":"Nathan@yesenia.net"},{"id":4,"name":"Patricia Lebsack","username":"Karianne","email":"Julianne.OConner@kory.org"},{"id":5,"name":"Chelsey Dietrich","username":"Kamren","email":"Lucio_Hettinger@annie.ca"}]
+  displayedColumns: string[] = ['name', 'username', 'email'];
+  enableTbl = false
+
+  constructor( private snackBar:MatSnackBar) {
+    setTimeout(() => {
+      this.enableTbl = true
+    }, 4000)
+  }
+  onFinish(event:any) {
+    console.log(event.message)
+  }
+
+  openAutoCloseToast(message: string) {
+    this.snackBar.open(message, '', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 3000
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 import { AppComponent } from './app.component';
-
+import { TableExportCopyModule } from "table-export-copy";
+import { MatTableModule } from "@angular/material/table";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TableExportCopyModule,
+    MatTableModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
